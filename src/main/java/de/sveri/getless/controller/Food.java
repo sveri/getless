6464578d54ec6c1172e40ced07dea74cca7cea4b
sveri <sveri@sveri.de>
2016-07-19 23:20:11 +0200

    show list of foods

--- src/main/java/de/sveri/getless/controller/Food.java
+++ src/main/java/de/sveri/getless/controller/Food.java
@@ -1,14 +1,37 @@
 package de.sveri.getless.controller;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import de.sveri.getless.entity.mongodb.ProductRepository;
 
 @Controller
 public class Food {
 	
+	@Autowired
+	ProductRepository productRepository;
+	
+	@RequestMapping(value = "/food/product/{id}")
+	public ModelAndView foodDetail(@PathVariable String id){
+		ModelAndView mav = new ModelAndView("food/detail");
+		
+		mav.addObject("product", productRepository.findOne(id));
+		return mav;
+	}
+	
 	@RequestMapping(value = "/food")
-	public String dietDiary(){
-		return "food/index";
+	public ModelAndView foodIndex(@RequestParam(name = "search-for", required = false, defaultValue = "") String searchFor){
+		ModelAndView mav = new ModelAndView("food/index");
+		
+		if(StringUtils.isNotEmpty(searchFor)){
+			mav.addObject("products", productRepository.findByNameLike(searchFor));
+		}
+		return mav;
 	}
 
 }
